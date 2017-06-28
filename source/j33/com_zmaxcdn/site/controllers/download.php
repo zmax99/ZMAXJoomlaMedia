5de61cf64193877b19234c4fe1e1103f086a704a
zmax <zhang19min88@163.com>
2017-06-28 16:50:54 +0800

    重写了下载模块，支持超大文件的下载

--- source/j33/com_zmaxcdn/site/controllers/download.php
+++ source/j33/com_zmaxcdn/site/controllers/download.php
@@ -1,25 +1,21 @@
-<?php 
+<?php
 /**
- *	description:ZMAX媒体管理组件  资料下载控制器
+ *	description:ZMAX媒体管理组件 资料下载控制器
  *  author：min.zhang
  *  Email:zhang19min88@163.com
  *	Url:http://www.zmax99.com
  *  copyright:南宁市程序人软件科技有限责任公司保留所有权利
- *  date:2015-10-11
- * 	@license GNU General Public License version 3, or later
+ *  date:2015-08-24
  *  check date:2016-07-11
  *  checker :min.zhang
- *  modified:min.zhang
- *  modify date:2017-06-21
+ *  @license GNU General Public License version 3, or later
  */
 defined('_JEXEC') or die('you can not access this file!');
 
-jimport('joomla.application.component.controlleradmin');
-require_once(JPATH_ROOT."/administrator/components/com_zmaxcdn/libs/zmaxcdn/item.php");
-	
+jimport('joomla.application.component.controlleradmin');	
 class zmaxcdnControllerDownload extends JControllerAdmin
 {
-
+ 	
 	public function download()
 	{
 		$app =  JFactory::getApplication();
@@ -30,24 +26,97 @@ class zmaxcdnControllerDownload extends JControllerAdmin
 		$app->close();
 	}
 
-	public function getFile()
+	public  function getFile()
 	{
+		//调整PHP的设置为下载做准备
+		// For a certain unmentionable browser
+		if (function_exists('ini_get') && function_exists('ini_set'))
+		{
+			if (ini_get('zlib.output_compression'))
+			{
+				ini_set('zlib.output_compression', 'Off');
+			}
+		}
+		
+		// Remove php's time limit 
+		if (function_exists('ini_get') && function_exists('set_time_limit'))
+		{
+			if (!ini_get('safe_mode'))
+			{
+				@set_time_limit(0);
+			}
+		}
+		
+		//获得需要下载的资源
 		$app =  JFactory::getApplication();
 		$id = $app->input->get("id","","INT");
 		$item = zmaxcdnitemHelper::getItemById($id);
-		$file = zmaxcdnitemhelper::getItemUrl($item);
-		ob_clean();
-		header('Content-Description: File Transfer');
-		header("Content-type:".$item->type);
-		header('Content-Transfer-Encoding: binary');
-		header('Expires: 0');
-		//设置文件的下载信息
-		header("Content-Disposition:attachment;filename=".$item->name);
-		header('Pragma: public');
-        header('Content-Length: ' .$item->size);
-		readfile($file);
-		exit();
-		$app->close();		
+		$filename = zmaxcdnitemhelper::getItemUrl($item);
+		
+		//开始执行下载
+		$basename	 = @basename($filename);
+		$filesize	 = @filesize($filename);
+		$extension	 = strtolower(str_replace(".", "", strrchr($filename, ".")));
+		
+		$basename = $item->name;
+		
+		while (@ob_end_clean());
+			@clearstatcache();
+			// Send MIME headers
+			header('MIME-Version: 1.0');
+			header('Content-Disposition: attachment; filename="' . $basename . '"');
+			header('Content-Transfer-Encoding: binary');
+			header('Accept-Ranges: bytes');
+
+			switch ($extension)
+			{
+				case 'zip':
+					// ZIP MIME type
+					header('Content-Type: application/zip');
+					break;
+
+				default:
+					// Generic binary data MIME type
+					header('Content-Type: application/octet-stream');
+					break;
+			}
+
+			// Notify of filesize, if this info is available
+			if ($filesize > 0)
+				header('Content-Length: ' . @filesize($filename));
+			// Disable caching
+			header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+			header("Expires: 0");
+			header('Pragma: no-cache');
+
+			flush();
+
+			if ($filesize > 0)
+			{
+				// If the filesize is reported, use 1M chunks for echoing the data to the browser
+				$blocksize	 = 1048756; //1M chunks
+				$handle		 = @fopen($filename, "r");
+				// Now we need to loop through the file and echo out chunks of file data
+				if ($handle !== false)
+					while (!@feof($handle))
+					{
+						echo @fread($handle, $blocksize);
+						@ob_flush();
+						flush();
+					}
+
+				if ($handle !== false)
+				{
+					@fclose($handle);
+				}
+			}
+			else
+			{
+				// If the filesize is not reported, hope that readfile works
+				@readfile($filename);
+			}
+
+			exit(0);
 	}
  
  }	
